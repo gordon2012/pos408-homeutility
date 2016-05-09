d70a803061bb73c04ece68f62b52288316d03f0f
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-05-08 23:27:37 -0700

    initial commit

---
+++ pos408-homeutility/MainForm.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmbAppliance = New System.Windows.Forms.ComboBox()
+        Me.txtPower = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtHours = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtCost = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(298, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Home Utility Auditing Program"
+        '
+        'cmbAppliance
+        '
+        Me.cmbAppliance.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbAppliance.FormattingEnabled = True
+        Me.cmbAppliance.Location = New System.Drawing.Point(12, 116)
+        Me.cmbAppliance.Name = "cmbAppliance"
+        Me.cmbAppliance.Size = New System.Drawing.Size(193, 28)
+        Me.cmbAppliance.TabIndex = 1
+        '
+        'txtPower
+        '
+        Me.txtPower.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPower.Location = New System.Drawing.Point(234, 118)
+        Me.txtPower.Name = "txtPower"
+        Me.txtPower.Size = New System.Drawing.Size(164, 26)
+        Me.txtPower.TabIndex = 2
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(347, 177)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(125, 29)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(230, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(150, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Power Needed (kW)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(52, 74)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Appliance"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(427, 74)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(154, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Hours Used Per Day"
+        '
+        'txtHours
+        '
+        Me.txtHours.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtHours.Location = New System.Drawing.Point(431, 118)
+        Me.txtHours.Name = "txtHours"
+        Me.txtHours.Size = New System.Drawing.Size(150, 26)
+        Me.txtHours.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(271, 243)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(80, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Daily Cost"
+        '
+        'txtCost
+        '
+        Me.txtCost.Enabled = False
+        Me.txtCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCost.Location = New System.Drawing.Point(417, 240)
+        Me.txtCost.Name = "txtCost"
+        Me.txtCost.Size = New System.Drawing.Size(164, 26)
+        Me.txtCost.TabIndex = 9
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(803, 519)
+        Me.Controls.Add(Me.txtCost)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtHours)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtPower)
+        Me.Controls.Add(Me.cmbAppliance)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "MainForm"
+        Me.Text = "Home Utility Auditing Program"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmbAppliance As System.Windows.Forms.ComboBox
+    Friend WithEvents txtPower As System.Windows.Forms.TextBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtHours As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtCost As System.Windows.Forms.TextBox
+
+End Class
---
+++ pos408-homeutility/MainForm.vb
@@ -0,0 +1,48 @@
+﻿Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Initialize appliance names and min max values
+
+        Dim applianceList As New Dictionary(Of Double, String)()
+
+        applianceList.Add(0.11, "Refrigerator")
+
+        cmbAppliance.DataSource = New BindingSource(applianceList, Nothing)
+        cmbAppliance.DisplayMember = "Value"
+        cmbAppliance.ValueMember = "Key"
+
+
+
+
+
+
+
+
+
+        'txtCost.Text = "Test1"
+
+        'cmbAppliance.
+
+        'Refrigerator
+        'Hair Dryer
+        'Fan
+        'Microwave
+        'Computer
+        'Television
+
+
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        ' Validate cost value
+
+
+        ' Calculate daily cost
+
+        ' MessageBox.Show()
+        ' TODO: test to make sure key and valus work
+
+    End Sub
+End Class
