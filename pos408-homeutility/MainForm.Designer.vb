32e2d255f6176513ef73b48ee653e418b5652006
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-05-13 01:32:55 -0700

    complete main functionality

--- pos408-homeutility/MainForm.Designer.vb
+++ pos408-homeutility/MainForm.Designer.vb
@@ -24,24 +24,25 @@ Partial Class MainForm
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.cmbAppliance = New System.Windows.Forms.ComboBox()
-        Me.txtPower = New System.Windows.Forms.TextBox()
+        Me.txtRating = New System.Windows.Forms.TextBox()
         Me.btnCalculate = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.txtHours = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtDaily = New System.Windows.Forms.TextBox()
         Me.txtCost = New System.Windows.Forms.TextBox()
-        Me.txtRating = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(75, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(298, 25)
+        Me.Label1.Size = New System.Drawing.Size(447, 37)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Home Utility Auditing Program"
         '
@@ -54,20 +55,20 @@ Partial Class MainForm
         Me.cmbAppliance.Size = New System.Drawing.Size(193, 28)
         Me.cmbAppliance.TabIndex = 1
         '
-        'txtPower
+        'txtRating
         '
-        Me.txtPower.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtPower.Location = New System.Drawing.Point(234, 118)
-        Me.txtPower.Name = "txtPower"
-        Me.txtPower.Size = New System.Drawing.Size(164, 26)
-        Me.txtPower.TabIndex = 2
+        Me.txtRating.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRating.Location = New System.Drawing.Point(235, 116)
+        Me.txtRating.Name = "txtRating"
+        Me.txtRating.Size = New System.Drawing.Size(193, 26)
+        Me.txtRating.TabIndex = 2
         '
         'btnCalculate
         '
         Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCalculate.Location = New System.Drawing.Point(347, 177)
+        Me.btnCalculate.Location = New System.Drawing.Point(235, 259)
         Me.btnCalculate.Name = "btnCalculate"
-        Me.btnCalculate.Size = New System.Drawing.Size(125, 29)
+        Me.btnCalculate.Size = New System.Drawing.Size(193, 29)
         Me.btnCalculate.TabIndex = 3
         Me.btnCalculate.Text = "Calculate"
         Me.btnCalculate.UseVisualStyleBackColor = True
@@ -76,11 +77,11 @@ Partial Class MainForm
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(230, 74)
+        Me.Label2.Location = New System.Drawing.Point(266, 74)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(150, 20)
+        Me.Label2.Size = New System.Drawing.Size(141, 20)
         Me.Label2.TabIndex = 4
-        Me.Label2.Text = "Power Needed (kW)"
+        Me.Label2.Text = "Power Rating (kW)"
         '
         'Label3
         '
@@ -96,7 +97,7 @@ Partial Class MainForm
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(427, 74)
+        Me.Label4.Location = New System.Drawing.Point(482, 74)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(154, 20)
         Me.Label4.TabIndex = 6
@@ -105,53 +106,64 @@ Partial Class MainForm
         'txtHours
         '
         Me.txtHours.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtHours.Location = New System.Drawing.Point(431, 118)
+        Me.txtHours.Location = New System.Drawing.Point(467, 116)
         Me.txtHours.Name = "txtHours"
-        Me.txtHours.Size = New System.Drawing.Size(150, 26)
+        Me.txtHours.Size = New System.Drawing.Size(193, 26)
         Me.txtHours.TabIndex = 7
         '
         'Label5
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(271, 243)
+        Me.Label5.Location = New System.Drawing.Point(520, 214)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(80, 20)
         Me.Label5.TabIndex = 8
         Me.Label5.Text = "Daily Cost"
         '
+        'txtDaily
+        '
+        Me.txtDaily.Enabled = False
+        Me.txtDaily.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDaily.Location = New System.Drawing.Point(467, 262)
+        Me.txtDaily.Name = "txtDaily"
+        Me.txtDaily.ReadOnly = True
+        Me.txtDaily.Size = New System.Drawing.Size(193, 26)
+        Me.txtDaily.TabIndex = 9
+        '
         'txtCost
         '
-        Me.txtCost.Enabled = False
         Me.txtCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCost.Location = New System.Drawing.Point(417, 240)
+        Me.txtCost.Location = New System.Drawing.Point(12, 259)
         Me.txtCost.Name = "txtCost"
-        Me.txtCost.Size = New System.Drawing.Size(164, 26)
-        Me.txtCost.TabIndex = 9
+        Me.txtCost.Size = New System.Drawing.Size(193, 26)
+        Me.txtCost.TabIndex = 11
         '
-        'txtRating
+        'Label6
         '
-        Me.txtRating.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtRating.Location = New System.Drawing.Point(12, 150)
-        Me.txtRating.Name = "txtRating"
-        Me.txtRating.ReadOnly = True
-        Me.txtRating.Size = New System.Drawing.Size(193, 26)
-        Me.txtRating.TabIndex = 10
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(61, 214)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(97, 20)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Price (cents)"
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(803, 519)
-        Me.Controls.Add(Me.txtRating)
+        Me.ClientSize = New System.Drawing.Size(701, 347)
+        Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.txtCost)
+        Me.Controls.Add(Me.txtDaily)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.txtHours)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.btnCalculate)
-        Me.Controls.Add(Me.txtPower)
+        Me.Controls.Add(Me.txtRating)
         Me.Controls.Add(Me.cmbAppliance)
         Me.Controls.Add(Me.Label1)
         Me.Name = "MainForm"
@@ -162,14 +174,15 @@ Partial Class MainForm
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents cmbAppliance As System.Windows.Forms.ComboBox
-    Friend WithEvents txtPower As System.Windows.Forms.TextBox
+    Friend WithEvents txtRating As System.Windows.Forms.TextBox
     Friend WithEvents btnCalculate As System.Windows.Forms.Button
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents txtHours As System.Windows.Forms.TextBox
     Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtDaily As System.Windows.Forms.TextBox
     Friend WithEvents txtCost As System.Windows.Forms.TextBox
-    Friend WithEvents txtRating As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
 
 End Class
--- pos408-homeutility/MainForm.vb
+++ pos408-homeutility/MainForm.vb
@@ -1,32 +1,25 @@
 ﻿Public Class MainForm
+
+    ' TODO: title
+
+
+
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         ' Initialize appliance names and base values from Appliancefigures.xls
 
         Dim applianceList As New Dictionary(Of String, Double)()
 
-        ' applianceList.Add("Electric Wok", 1.2)
-        ' applianceList.Add("Coffee maker", 0.8)
-        ' applianceList.Add("Hot plate", 1.2)
         applianceList.Add("Refrigerator", 0.11)
-        ' applianceList.Add("Deep freezer", 0.19)
         applianceList.Add("Hair dryer", 1.5)
         applianceList.Add("Television", 0.31)
-        ' applianceList.Add("Stereo", 0.15)
         applianceList.Add("Desktop computer", 0.09)
         applianceList.Add("Fan", 0.2)
-        ' applianceList.Add("Space heater", 1.5)
-        ' applianceList.Add("Washing machine", 0.45)
-        ' applianceList.Add("Clothes dryer", 2.7)
-        ' applianceList.Add("Water heater ", 0.56)
-        ' applianceList.Add("Dehumidifier", 0.76)
         applianceList.Add("Microwave(1000W)", 1.0)
-        ' applianceList.Add("Toaster", 0.9)
 
         cmbAppliance.DataSource = New BindingSource(applianceList, Nothing)
         cmbAppliance.DisplayMember = "Key"
         cmbAppliance.ValueMember = "Value"
-
 
         txtRating.Text = cmbAppliance.SelectedValue.ToString()
 
@@ -35,15 +28,32 @@
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
 
         ' Validate cost value
+        ' TODO: limit rating range, hours per day, and cost based on reasonable amounts
 
+        Dim success As Boolean = True
 
-        ' Calculate daily cost
+        Dim rating As Double = 0
+        Dim hours As Double = 0
+        Dim cost As Double = 0
 
+        ' TODO: fix later trues overwrite previous falses
+        '
+        success = Double.TryParse(txtRating.Text, rating)
+        success = Double.TryParse(txtHours.Text, hours)
+        success = Double.TryParse(txtCost.Text, cost)
 
+        If success Then
+            ' Calculate daily cost
+            txtDaily.Text = FormatCurrency(rating * hours * (cost / 100))
+        Else
+            txtRating.Text = cmbAppliance.SelectedValue.ToString()
+            txtHours.Text = ""
+            txtCost.Text = ""
+            txtDaily.Text = ""
+        End If
     End Sub
 
     Private Sub cmbAppliance_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbAppliance.SelectedIndexChanged
         txtRating.Text = cmbAppliance.SelectedValue.ToString()
-
     End Sub
 End Class
